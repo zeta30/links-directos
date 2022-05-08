4d6a134a9bac272917dae77aaec773c60826700a
Hacke <102891240+xhoho25@users.noreply.github.com>
2022-05-07 23:49:58 -0400

    Create conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "3be954be110a796e8c0b18f3604a5f2c"
 api_id = 16356512
 bot_token = "5158342939:AAFvj3C7V8Q0_md4qROXstHs6VxweUL-a3A"
-bot_admin_group = -643164238
+bot_admin_group = -1001545290821
