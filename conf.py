6e621c5c952f3ed094a631b4dda9009c5eaf2f0a
AleJefe36 <100821686+AleJefe36@users.noreply.github.com>
2022-05-12 17:27:43 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "3be954be110a796e8c0b18f3604a5f2c"
 api_id = 16356512
-bot_token = "5245905555:AAEwF8F8B5hxyFKg1ReOBeb2B6fonye7y94"
-bot_admin_group = -701147679
+bot_token = "5131516659:AAHfssJ2XTFKSGEZJJQBdPW4FmmmTnafF6M"
+bot_admin_group = -687583366
